382c6b2b7b6dcd3ad1e798de144ea5ace7df53fe
Serhan <35522745+SerhanTRGL@users.noreply.github.com>
2024-01-30 13:18:37 +0200

    Added simple homescreen view and urls to base

--- todolist/urls.py
+++ todolist/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+from base.views import (
+    home_screen_view
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", include('base.urls'))
 ]
