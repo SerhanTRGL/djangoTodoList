0f39765c2e71b834d5053e9fbb1bd9e35096a14c
Serhan <35522745+SerhanTRGL@users.noreply.github.com>
2024-01-30 11:57:36 +0200

    Added TodoList to models

--- base/models.py
+++ base/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class TodoList(models.Model):
+    title = models.CharField(max_length=256)
+    description = models.TextField()
+    startDate = models.DateTimeField()
+    itemID = models.BigAutoField(primary_key=True)
